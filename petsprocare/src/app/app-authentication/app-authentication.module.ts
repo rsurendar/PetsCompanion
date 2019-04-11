d464d623e837a0fa7148a9907348d6c175943a36
rsurendar <rsurendar26@gmail.com>
2019-04-11 23:10:40 +0530

    petsprocare modules

---
+++ petsprocare/src/app/app-authentication/app-authentication.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppAuthenticationRoutingModule } from './app-authentication-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppAuthenticationRoutingModule
+  ]
+})
+export class AppAuthenticationModule { }
---
+++ petsprocare/src/app/app-customer/app-customer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppCustomerRoutingModule } from './app-customer-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppCustomerRoutingModule
+  ]
+})
+export class AppCustomerModule { }
